bf44e53ba5a445f540cb0347c274b8b1220c9655
practicanteit2 <practicanteit2@CPU_41024.PROFAMILIA.NET>
2023-04-17 13:39:42 -0500

    Update

---
+++ Intento/src/intento/intento.java
@@ -0,0 +1,9 @@
+package intento;
+
+public class intento {
+	public static void main(String[] args) {
+		System.err.println("Hola mundo");
+
+	}
+
+}
---
+++ Intento/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author practicanteit2
+ *
+ */
+module Intento {
+}
